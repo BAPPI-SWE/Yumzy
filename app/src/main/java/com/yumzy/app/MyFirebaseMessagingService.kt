29172a1bd30a08e3a261b1704d2484b613a39ded
Rakibul islam Bappi <bappi616@gmail.com>
2025-08-03 19:33:57 +0600

    comment

--- app/src/main/java/com/yumzy/app/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/yumzy/app/MyFirebaseMessagingService.kt
@@ -10,7 +10,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-
+ //comment
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
